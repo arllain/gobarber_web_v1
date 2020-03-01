acf938e8db0bda32745da7189dbd8510a837124c
arllainm <arllain@gmail.com>
2020-03-01 13:37:57 -0300

    Using Unform lib

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input placeholder="Full name" />
-        <input type="email" placeholder="Your e-mail" />
-        <input type="password" placeholder="Your passwaord" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="fullname" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input name="password" type="password" placeholder="Your passwaord" />
         <button type="submit">Create accout</button>
         <Link to="/">I already have an account</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
 
 export default function Signin() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input type="email" placeholder="Your e-mail" />
-        <input type="password" placeholder="Your passwaord" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input name="password" type="password" placeholder="Your passwaord" />
         <button type="submit">Sign in</button>
         <Link to="/register">Create new account</Link>
-      </form>
+      </Form>
     </>
   );
 }
