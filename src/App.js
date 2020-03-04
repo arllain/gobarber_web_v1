5ff5c673532fb439fd2d995246e62fe6cd9d527c
arllainm <arllain@gmail.com>
2020-03-03 21:18:47 -0300

    Authentication persisting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 import GlocalStyle from './styles/global';
-import store from './store';
+import { store, persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlocalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlocalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
