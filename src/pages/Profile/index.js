cb2f1d224ef42fc735d0bb894833c72075b237c8
arllainm <arllain@gmail.com>
2020-03-06 09:56:38 -0300

    Updating profile picture

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import { updateProfileRequest } from '~/store/modules/user/actions';
+import AvatarInput from './AvatarInput';
 
 import { Container } from './styles';
 
@@ -10,22 +11,24 @@ export default function Profile() {
   const profile = useSelector(state => state.user.profile);
 
   function handleSubmit(data) {
+    console.tron.log(`profile -> ${data}`);
     dispatch(updateProfileRequest(data));
   }
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
         <Input name="name" placeholder="Full name" />
         <Input name="email" type="email" placeholder="your email" />
         <hr />
         <Input
-          name="password"
+          name="oldpassword"
           type="oldpassword"
           placeholder="your actual password"
         />
-        <Input name="password" type="password" placeholder="new password" />
+        <Input name="newpassword" type="password" placeholder="new password" />
         <Input
-          name="password"
+          name="confirmpassword"
           type="confirmpassword"
           placeholder="confirm new password"
         />
